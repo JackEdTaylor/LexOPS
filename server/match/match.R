4b2a3345db872a84c1c2ee12432c4d93cbda3b08
Jack Taylor <jackt@psy.gla.ac.uk>
2018-11-23 18:17:04 +0000

    hugely simplified matching algorithm

---
+++ server/match/match.R
@@ -0,0 +1,60 @@
+resultsdata <- reactive({
+  
+  # add similarity information to the lexops df
+  if (input$check.os) {
+    lexops$OS <- as.integer(switch(input$os.opt,
+                                   'ld'=vwr::levenshtein.distance(input$string, lexops$string),
+                                   'ldd'=vwr::levenshtein.damerau.distance(input$string, lexops$string)))
+  }
+  
+  if (input$check.ps) {
+    lexops$PS <- as.integer(switch(input$ps.opt,
+                                   'ld'=vwr::levenshtein.distance(lexops[[sprintf("CMU.pr%i_1letter", get_pron_nr(input$manual.pron.ps, input$string))]][lexops$string==input$string], lexops$CMU.pr1_1letter),
+                                   'ldd'=vwr::levenshtein.damerau.distance(lexops[[sprintf("CMU.pr%i_1letter", get_pron_nr(input$manual.pron.ps, input$string))]][lexops$string==input$string], lexops$CMU.pr1_1letter)))
+  }
+  
+  matched <- select(lexops, string) %>%
+    # Lexical
+    matcher(lexops, corpus_recode(input$frequency.opt, if(input$frequency.log){"Zipf"}else{"fpmw"}), input$frequency.sl, input$check.frequency, input$string, colmeans_name=sprintf("Avg.%s", if(input$frequency.log){"Zipf"}else{"fpmw"})) %>%
+    matcher(lexops, corpus_recode(input$pos.opt, "PoS"), NA, input$check.partofspeech, input$string, manual_str_in_x=if(input$check.manual.pos){input$manual.pos}else{NA}) %>%
+    # Orthographic
+    matcher(lexops, "Length", input$length.sl, input$check.length, input$string) %>%
+    matcher(lexops, corpus_recode(input$bgfreq.opt, "BG"), input$bgfreq.sl, input$check.bgfreq, input$string, colmeans_name=("Avg.BG")) %>%
+    matcher(lexops, sprintf("ON.%s%s", if(input$on.log){"Log_"}else{""}, switch(input$on.opt, "old20"="OLD20", "cn"="Colthearts_N")), input$on.sl, input$check.on, input$string) %>%
+    matcher(lexops, "OS", input$os.sl, input$check.os, input$string) %>%
+    # Phonological
+    matcher(lexops, corpus_recode(input$syllables.opt, "Syllables"), input$syllables.sl, input$check.syllables, input$string, pron_nr=if(input$syllables.opt=='cmu'){get_pron_nr(input$manual.pron.phonemes, input$string)}else{NA}) %>%
+    matcher(lexops, "Phonemes.CMU.pr1", input$phonemes.sl, input$check.phonemes, input$string, pron_nr=get_pron_nr(input$manual.pron.phonemes, input$string)) %>%
+    matcher(lexops, "Rhyme.CMU.pr1", NA, input$check.rhyme, input$string, pron_nr=get_pron_nr(input$manual.pron.rhyme, input$string)) %>%
+    matcher(lexops, sprintf("PN.%s%s.CMU.pr1", if(input$pn.log){"Log_"}else{""}, switch(input$pn.opt, "pld20"="PLD20", "cn"="Colthearts_N")), input$pn.sl, input$check.pn, input$string, pron_nr=get_pron_nr(input$manual.pron.pn, input$string)) %>%
+    matcher(lexops, "PS", input$ps.sl, input$check.ps, input$string) %>%
+    matcher(lexops, "CMU.PrN", input$prn.sl, input$check.prn, input$string) %>%
+    # Semantic
+    matcher(lexops, corpus_recode(input$fam.opt, "FAM"), input$fam.sl, input$check.fam, input$string) %>%
+    matcher(lexops, corpus_recode(input$aoa.opt, "AoA"), input$aoa.sl, input$check.aoa, input$string) %>%
+    matcher(lexops, corpus_recode(input$cnc.opt, "CNC"), input$cnc.sl, input$check.cnc, input$string) %>%
+    matcher(lexops, corpus_recode(input$imag.opt, "IMAG"), input$imag.sl, input$check.imag, input$string) %>%
+    matcher(lexops, corpus_recode(input$arou.opt, "AROU"), input$arou.sl, input$check.arou, input$string) %>%
+    matcher(lexops, corpus_recode(input$val.opt, "VAL"), input$val.sl, input$check.val, input$string) %>%
+    matcher(lexops, corpus_recode(input$dom.opt, "DOM"), input$dom.sl, input$check.dom, input$string) %>%
+    matcher(lexops, corpus_recode(input$size.opt, "SIZE"), input$size.sl, input$check.size, input$string) %>%
+    matcher(lexops, corpus_recode(input$gen.opt, "GEND"), input$gen.sl, input$check.gen, input$string) %>%
+    matcher(lexops, corpus_recode(input$rt.opt, if(input$rt.zscore){"RT_zscore"}else{"RT"}), input$rt.sl, input$check.rt, input$string) %>%
+    matcher(lexops, corpus_recode(input$acc.opt, if(input$acc.zscore){"Accuracy_zscore"}else{"Accuracy"}), input$acc.sl, input$check.acc, input$string)
+  
+  # Get differences and distances
+  matched_differences <- get_differences(matched, str_in=input$string)
+  matched_distances <- get_distances(matched_differences)
+  
+  # Return the results
+  if (input$results.format=='rv') {
+    res <- matched
+  } else if (input$results.format=='diff') {
+    res <- matched_differences
+  } else if (input$results.format=='dist') {
+    res <- matched_distances
+  }
+  
+  res
+  
+})
---
+++ server/match/matcher_functions.R
@@ -0,0 +1,98 @@
+# These functions serve to match by selected variables, and calculate the distances and differences from that associated with the target string
+
+matcher <- function(df, rawdf, column="Length", sl, checkbox, str_in, manual_str_in_x=NA, colmeans_name=NA, pron_nr=NA) {
+  if (checkbox) {
+    if (length(column)>1) {
+      # create new column, which will be the average of the variables selected
+      rawdf[[colmeans_name]] <- rowMeans(select(rawdf, one_of(column)), dims=1, na.rm=T)
+      column <- colmeans_name
+    }
+    
+    # add column to the df being built
+    df <- inner_join(df, select(rawdf, "string", column), by="string")
+    
+    if (is.na(pron_nr)) {
+      column_str_in_x <- column
+    } else {
+      column_str_in_x <- sprintf("%s%i", substr(column, 0, nchar(column)-1), pron_nr)
+    }
+    # get column's value for this string
+    if (is.na(manual_str_in_x)) {
+      str_in_x <- rawdf[[column_str_in_x]][rawdf$string==str_in]
+    } else {
+      str_in_x <- manual_str_in_x
+    }
+    if (is.numeric(rawdf[[column]])) {
+      # match numeric
+      limits <- sl + str_in_x
+      rawdf <- filter(rawdf, between(!!sym(column), limits[1], limits[2]) | string==str_in)
+    } else {
+      # match categorical
+      rawdf <- filter(rawdf, !!sym(column) == str_in_x | string==str_in)
+    }
+    
+    df %>% filter(string %in% rawdf$string)
+    
+  } else {
+    
+    df
+    
+  }
+}
+
+corpus_recode <- function(inputopts = c("bnc_w", "bnc_s"), prefix=NA) {
+  prefix_dot <- if(is.na(prefix)) {""} else {sprintf("%s.", prefix)}
+  recoded <- recode(inputopts,
+         "bnc_w" = "BNC.Written",
+         "bnc.wbg" = "BNC.Written",
+         "bnc_s" = "BNC.Spoken",
+         "bnc.sbg" = "BNC.Spoken",
+         "suk" = "SUBTLEX_UK",
+         "subtlex_uk.bg" = "SUBTLEX_UK",
+         "sus" = "SUBTLEX_US",
+         "subtlex_us.bg" = "SUBTLEX_US",
+         "elp" = "ELP",
+         "blp" = "BLP",
+         "mp" = "Moby",
+         "cmu" = "CMU.pr1",
+         "gn" = "Glasgow_Norms",
+         "cp" = "Clark_and_Paivio",
+         "kuperman" = "Kuperman",
+         "brysbaert" = "Brysbaert",
+         "warriner" = "Warriner"
+         )
+  sprintf("%s%s", prefix_dot, recoded)
+}
+
+get_differences <- function(df=matched, str_in) {
+  get_diff <- function(df1=df, str_in1=str_in, column="Length") {
+    if (is.numeric(df1[[column]])) {
+      str_in_x <- df1[[column]][df1$string==str_in1]
+      df1[[column]] - str_in_x
+    } else {
+      df1[[column]]
+    }
+  }
+  out <- df
+  for (cname in names(out)) {
+    out[[cname]] <- get_diff(column=cname)
+  }
+  out
+}
+
+get_distances <- function(df=matched_differences) {
+  # for speed, this function is run on matched_differences
+  get_dist <- function(coldata) {
+    if (is.numeric(coldata)) {
+      abs(coldata)
+    } else {
+      coldata
+    }
+  }
+  out <- df
+  for (cname in names(out)) {
+    out[[cname]] <- get_dist(out[[cname]])
+  }
+  out
+}
+
