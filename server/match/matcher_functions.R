6bf44af140ff6a9f3e7e7905058f6cafd3fafede
Jack Taylor <jackt@psy.gla.ac.uk>
2018-12-11 17:51:00 +0000

    support for averaging across semantic features from different sources

--- server/match/matcher_functions.R
+++ server/match/matcher_functions.R
@@ -1,7 +1,12 @@
 # These functions serve to match by selected variables, and calculate the distances and differences from that associated with the target string
 
-matcher <- function(df, rawdf, column="Length", sl, checkbox, str_in, manual_str_in_x=NA, colmeans_name=NA, pron_nr=NA) {
-  if (checkbox) {
+matcher <- function(df, rawdf, column="Length", sl, checkbox, str_in, manual_str_in_x=NA, colmeans_name=NA, pron_nr=NA, scale.cols=F) {
+  if (checkbox & !(is.null(column)|is.na(column))) {
+    
+    if (scale.cols) {
+      rawdf[column] <- lapply(rawdf[column], scale)
+    }
+    
     if (length(column)>1) {
       # create new column, which will be the average of the variables selected
       rawdf[[colmeans_name]] <- rowMeans(select(rawdf, one_of(column)), dims=1, na.rm=T)
@@ -41,29 +46,33 @@ matcher <- function(df, rawdf, column="Length", sl, checkbox, str_in, manual_str
 }
 
 corpus_recode <- function(inputopts = c("bnc_w", "bnc_s"), prefix=NA) {
-  prefix_dot <- if(is.na(prefix)) {""} else {sprintf("%s.", prefix)}
-  recoded <- recode(inputopts,
-         "bnc_w" = "BNC.Written",
-         "bnc.wbg" = "BNC.Written",
-         "bnc_s" = "BNC.Spoken",
-         "bnc.sbg" = "BNC.Spoken",
-         "suk" = "SUBTLEX_UK",
-         "subtlex_uk.bg" = "SUBTLEX_UK",
-         "sus" = "SUBTLEX_US",
-         "subtlex_us.bg" = "SUBTLEX_US",
-         "elp" = "ELP",
-         "blp" = "BLP",
-         "mp" = "Moby",
-         "cmu" = "CMU.pr1",
-         "gn" = "Glasgow_Norms",
-         "cp" = "Clark_and_Paivio",
-         "kuperman" = "Kuperman",
-         "bb" = "BrysbaertBiemiller",
-         "brysbaert" = "Brysbaert",
-         "warriner" = "Warriner",
-         "eh" = "EngelthalerHills"
-         )
-  sprintf("%s%s", prefix_dot, recoded)
+  if (!is.null(inputopts)) {
+    prefix_dot <- if(is.na(prefix)) {""} else {sprintf("%s.", prefix)}
+    recoded <- recode(inputopts,
+                      "bnc_w" = "BNC.Written",
+                      "bnc.wbg" = "BNC.Written",
+                      "bnc_s" = "BNC.Spoken",
+                      "bnc.sbg" = "BNC.Spoken",
+                      "suk" = "SUBTLEX_UK",
+                      "subtlex_uk.bg" = "SUBTLEX_UK",
+                      "sus" = "SUBTLEX_US",
+                      "subtlex_us.bg" = "SUBTLEX_US",
+                      "elp" = "ELP",
+                      "blp" = "BLP",
+                      "mp" = "Moby",
+                      "cmu" = "CMU.pr1",
+                      "gn" = "Glasgow_Norms",
+                      "cp" = "Clark_and_Paivio",
+                      "kuperman" = "Kuperman",
+                      "bb" = "BrysbaertBiemiller",
+                      "brysbaert" = "Brysbaert",
+                      "warriner" = "Warriner",
+                      "eh" = "EngelthalerHills"
+    )
+    sprintf("%s%s", prefix_dot, recoded)
+  } else {
+    NA
+  }
 }
 
 get_differences <- function(df=matched, str_in) {
