8cb9d731bf1da925dd3bbb6238495eb28263a9be
Jack Taylor <33028594+Jack-Tay@users.noreply.github.com>
2019-01-07 17:56:57 +0000

    get all levels of the factorial design

--- server/generate/generate.R
+++ server/generate/generate.R
@@ -1,4 +1,17 @@
 genresults <- reactive({
+  
+  splits <- list()
+  for (i in 1:gen_splitby_boxes_N()) {
+    boxid <- sprintf('gen_splitby_%i', i)
+    Nlevels <- input[[sprintf("%s_Nlevels", boxid)]]
+    i_lttr <- LETTERS[i]
+    for (lvl in 1:Nlevels) {
+      splits[[i_lttr]] <- append(splits[[i_lttr]], sprintf("%s%i", i_lttr, lvl))
+    }
+  }
+  
+  levels <- expand.grid(splits)
+  
   lexops
 })
 
