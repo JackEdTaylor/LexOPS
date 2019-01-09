a8b6f870c10f730518171e36ac9a682ca28e6d2d
Jack Taylor <33028594+Jack-Tay@users.noreply.github.com>
2019-01-09 16:48:00 +0000

    generate algorithm improve and fix splitting into conditions add filter by

--- server/generate/generate.R
+++ server/generate/generate.R
@@ -1,8 +1,47 @@
 genresults <- reactive({
   
   res <- lexops %>%
-    select(string) %>%
-    mutate(Condition = NA)
+    select(string)
+  
+  # Filter by...
+  
+  lexops_filt <- lexops
+  
+  if (gen_filterby_boxes_N() >= 1) {
+    
+    for (i in 1:gen_filterby_boxes_N()) {
+      lexops_custom_cols <- lexops
+      boxid <- sprintf('gen_filterby_%i', i)
+      sl <- input[[sprintf('%s_sl', boxid)]]  # get the box's filter
+      boxlog <- if (is.null(input[[sprintf('%s.log', boxid)]])) {F} else {input[[sprintf('%s.log', boxid)]]}
+      boxopt <- if (is.null(input[[sprintf('%s.opt', boxid)]])) {""} else {input[[sprintf('%s.opt', boxid)]]}
+      boxv <- input[[sprintf('%s_vtype', boxid)]]
+      column <- corpus_recode_columns(boxopt, boxv, boxlog)
+      if (length(column)>1) {
+        # create new column, which will be the average of the variables selected
+        if (!(boxv %in% c("Word Frequency", "Bigram Probability"))) {
+          lexops_custom_cols[column] <- lapply(lexops_custom_cols[column], scale)
+        }
+        colmeans_name <- sprintf("Avg.%s", viscat2prefix(boxv, boxlog))
+        lexops_custom_cols[[colmeans_name]] <- rowMeans(select(lexops_custom_cols, one_of(column)), dims=1, na.rm=T)
+        lexops_filt <- inner_join(lexops_filt, select(lexops_custom_cols, "string", UQ(sym(colmeans_name))), by="string")
+        column <- colmeans_name
+      }
+      res[[column]] <- lexops_custom_cols[[column]]  # copy over the column to the results df
+      
+      if (is.numeric(lexops_filt[[column]])) {
+        lexops_filt <- filter(lexops_filt, UQ(sym(column)) >= sl[[1]] & UQ(sym(column)) <= sl[[2]])
+      } else {
+        lexops_filt <- filter(lexops_filt, UQ(sym(column)) %in% sl)
+      }
+    }
+  }
+  
+  res <- filter(res, string %in% lexops_filt$string)
+  
+  # Split by...
+  
+  res <- mutate(res, Condition = NA)
   
   splits <- list()
   for (i in 1:gen_splitby_boxes_N()) {
@@ -19,11 +58,11 @@ genresults <- reactive({
   levels <- expand.grid(splits)
   levels <- unite(levels, "Level", 1:ncol(levels), sep="_", remove=F)
   
-  lexops_custom_cols <- lexops
+  lexops_custom_cols <- lexops[lexops$string %in% res$string, ]
   
   for (lvl in 1:nrow(levels)) {
     rowlevel <- levels[lvl, ]
-    lexops_filt <- lexops
+    lexops_filt <- lexops[lexops$string %in% res$string, ]
     
     for (i in 1:gen_splitby_boxes_N()) {
       i_lttr <- LETTERS[i]
@@ -31,14 +70,17 @@ genresults <- reactive({
       boxid <- sprintf('gen_splitby_%i', i)
       sl <- input[[sprintf('%s_sl%i', boxid, i_lttr_lvl)]]  # get the level's filter
       boxlog <- if (is.null(input[[sprintf('%s.log', boxid)]])) {F} else {input[[sprintf('%s.log', boxid)]]}
-      boxopt <- input[[sprintf('%s.opt', boxid)]]
+      boxopt <- if (is.null(input[[sprintf('%s.opt', boxid)]])) {""} else {input[[sprintf('%s.opt', boxid)]]}
       boxv <- input[[sprintf('%s_vtype', boxid)]]
       column <- corpus_recode_columns(boxopt, boxv, boxlog)
       if (length(column)>1) {
         # create new column, which will be the average of the variables selected
+        if (!(boxv %in% c("Word Frequency", "Bigram Probability"))) {
+          lexops_custom_cols[column] <- lapply(lexops_custom_cols[column], scale)
+        }
         colmeans_name <- sprintf("Avg.%s", viscat2prefix(boxv, boxlog))
         lexops_custom_cols[[colmeans_name]] <- rowMeans(select(lexops_custom_cols, one_of(column)), dims=1, na.rm=T)
-        lexops_filt[[colmeans_name]] <- rowMeans(select(lexops_filt, one_of(column)), dims=1, na.rm=T)
+        lexops_filt <- inner_join(lexops_filt, select(lexops_custom_cols, "string", UQ(sym(colmeans_name))), by="string")
         column <- colmeans_name
       }
       if (!(column %in% colnames(res))) {
@@ -55,6 +97,7 @@ genresults <- reactive({
     res$Condition[res$string %in% lexops_filt$string] <- rowlevel$Level
   }
   
-  res %>%
-    filter(!is.na(Condition))
+  res <- filter(res, !is.na(Condition))
+  
+  res
 })
