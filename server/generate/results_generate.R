dfe7d578c21efc5489ee505387a31e242091d934
Jack Taylor <33028594+Jack-Tay@users.noreply.github.com>
2019-01-14 12:01:39 +0000

    UI options for controlling for variables

--- server/generate/results_generate.R
+++ server/generate/results_generate.R
@@ -10,3 +10,15 @@ output$generated.csv <- downloadHandler(
 output$gen_results_dt <- DT::renderDataTable({
   DT::datatable(genresults(), options=list(pageLength=25, scrollX=T))
 })
+
+# Options for controlling for variables
+output$gen_controlnull_choice <- renderUI({
+  if (gen_splitby_boxes_N() >= 1) {
+    cells <- genlevels() %>%
+      select(Level) %>%
+      unlist(use.names=F)
+  } else {
+    cells <- c("(None)")
+  }
+  selectInput('gen_controlnull', "Control for Variables relative to", c("All conditions (inclusive tolerance)"="inclusive", cells))
+})
