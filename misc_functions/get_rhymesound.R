16aa353f60066f08b3119f0ef8cd94adabf44bbe
Jack Taylor <33028594+Jack-Tay@users.noreply.github.com>
2019-02-01 15:32:29 +0000

    fix rhyme sound issue with double-vowel and double-consonant sounds, as in "lisp" (IH_S_P) or "museum" ("IY_AH_M")

--- misc_functions/get_rhymesound.R
+++ misc_functions/get_rhymesound.R
@@ -1,25 +1,26 @@
 # Requires phoneme representation to be in two-letter format
 
 get_rhymesound <- function(pronun_vec = c("AH", "D",  "ER", "L",  "IY"), with.stress = F) {
-  prev_sound <- ""
-  vowel_sound <- ""
   vowel_vec <- if (with.stress) {cmu_vowel_phons_stress} else {cmu_vowel_phons}
+  firstsound <- T
+  rhyme_sound <- ""
+  vowel_detected <- F
   for (sound in rev(pronun_vec)) {
-    if (sound %in% vowel_vec) {
-      vowel_sound <- sound
-      break
+    if (firstsound) {
+      rhyme_sound <- sound
+      firstsound <- F
     } else {
-      if (prev_sound != ""){
-        prev_sound <- sprintf("%s_%s", sound, prev_sound)
-      } else {
-        prev_sound <- sprintf("%s", sound)
+      if (!(sound %in% vowel_vec)) {
+        if (vowel_detected) {
+          break
+        }
+        rhyme_sound <- sprintf("%s_%s", sound, rhyme_sound)
+      }
+      if (sound %in% vowel_vec) {
+        rhyme_sound <- sprintf("%s_%s", sound, rhyme_sound)
       }
     }
+    if (sound %in% vowel_vec) {vowel_detected <- T}
   }
-  if (vowel_sound != ""){
-    sep <- if(prev_sound=="") {""} else {"_"}
-    sprintf("%s%s%s", vowel_sound, sep, prev_sound)
-  } else {
-    NA
-  }
+  rhyme_sound
 }
