38936b2fde612f521ed9a25f72c1486dd0cf8b02
Jack Taylor <33028594+Jack-Tay@users.noreply.github.com>
2019-02-25 18:09:45 +0000

    give distance tolerance with a numeric input (with sensible default) rather than with a slider

--- server/generate/results_generate.R
+++ server/generate/results_generate.R
@@ -37,7 +37,7 @@ output$gen_dist_opts_choice <- renderUI({
   if (input$gen_check.dist) {
     ui <- list()
     ui[[1]] <- column(12, radioButtons('gen_dist.opt', 'Distance Measure', c('Euclidean Distance'='ed', 'City Block Distance'='cb'), selected='ed'))
-    ui[[2]] <- column(12, sliderInput('gen_dist_tol', 'Distance Tolerance', min=0, max=50, value=1, step=0.1, width='100%'))
+    ui[[2]] <- column(12, numericInput('gen_dist_tol', 'Distance Tolerance', min=0, value=.005, step=0.1, width='100%'))
     ui
   } else {
     NULL
