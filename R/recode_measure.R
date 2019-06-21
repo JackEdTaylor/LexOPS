5f54b90347080addcd374435fb953258d0537f0e
Jack Taylor <jackt@psy.gla.ac.uk>
2019-06-21 14:09:41 +0100

    add title capitalisation argument

--- R/recode_measure.R
+++ R/recode_measure.R
@@ -5,6 +5,7 @@
 #' @param var The variable name (non-standard evaluation)
 #' @param first_cite Logical; if `TRUE`, gives full name (e.g. frequency per million words), if `FALSE`, gives abbreviated name (e.g. FPMW)
 #' @param default The character string that should be returned if the variable does not have a known cietable source
+#' @param title_caps Logical; use title capitalisation (e.g. "Familiarity Ratings" rather than "famliarity ratings"). Default is `FALSE`.
 #' @param standard_eval Logical; bypasses non-standard evaluation. If `TRUE`, `var` should be a column name in quotation marks. If `FALSE`, the quotation marks are not necessary. Default = `FALSE`.
 #'
 #' @return A citation of a variable's source in APA format. Returns `default` if not recognised as a citeabile source.
@@ -19,82 +20,92 @@
 #'
 #' @export
 
-recode_measure <- function(var, first_cite = TRUE, default = "", standard_eval = FALSE) {
+recode_measure <- function(var, first_cite = TRUE, default = "", title_caps = FALSE, standard_eval = FALSE) {
   if (!standard_eval) var <- substitute(var)
   var_name <- corpus_recode_name_measure(var)
   if (!is.null(var_name) & !is.na(var_name)) {
     if (first_cite) {
-      dplyr::recode(
+      out <- dplyr::recode(
         var_name,
-        ".1letter" = "1-letter (ARPABET) representations",
-        ".PrN" = "number of pronunciations",
-        ".br_IPA" = "International Phonetic Alphabet (IPA) representations of words' British pronunciations",
+        ".1letter" = "1-letter (ARPABET) Representations",
+        ".PrN" = "Number of Pronunciations",
+        ".br_IPA" = "International Phonetic Alphabet (IPA) Representations of Words' British Pronunciations",
         "Zipf." = "Frequency in Zipf (Zipf=log10(frequency per million)+3)",
         "fpmw." = "Frequency per Million words (FPMW)",
         "PoS." = "Part of Speech",
-        "Length" = "Length (number of characters)",
-        "BG." = "bigram probability",
+        "Length" = "Length (Number of Characters)",
+        "BG." = "Bigram Probability",
         "ON.OLD20" = "Orthographic Levenshtein Distance 20 (OLD20)",
         "ON.Colthearts_N" = "Coltheart's N",
         "ON.Log_OLD20" = "Log Orthographic Levenshtein Distance 20 (OLD20)",
         "ON.Log_Colthearts_N" = "Log Coltheart's N",
-        "Syllables." = "number of syllables",
-        "Phonemes." = "number of phonemes",
-        "Rhyme." = "rhyme",
-        "FAM." = "familiarity ratings",
-        "AoA." = "age of acquisition (AoA)",
-        "CNC." = "concreteness ratings",
-        "IMAG." = "imageability ratings",
-        "AROU." = "arousal ratings",
-        "VAL." = "valence ratings",
-        "DOM." = "dominance ratings",
-        "SIZE." = "semantic size ratings",
-        "GEND." = "gender ratings",
-        "HUM." = "humour ratings",
-        "RT." = "lexical decision response time (RT)",
-        "Accuracy." = "lexical decision accuracy",
-        "PREV." = "word prevalence",
-        "PK." = "proportion known",
+        "Syllables." = "Number of Syllables",
+        "Phonemes." = "Number of Phonemes",
+        "Rhyme." = "Rhyme",
+        "FAM." = "Familiarity Ratings",
+        "AoA." = "Age of Acquisition",
+        "CNC." = "Concreteness Ratings",
+        "IMAG." = "Imageability Ratings",
+        "AROU." = "Arousal Ratings",
+        "VAL." = "Valence Ratings",
+        "DOM." = "Dominance Ratings",
+        "SIZE." = "Semantic Size Ratings",
+        "GEND." = "Gender Ratings",
+        "HUM." = "Humour Ratings",
+        "RT." = "Lexical Decision Response Time (RT)",
+        "Accuracy." = "Lexical Decision Accuracy",
+        "PREV." = "Word Prevalence",
+        "PK." = "Proportion Known",
         .default = default
       )
     } else {
-      dplyr::recode(
+      out <- dplyr::recode(
         var_name,
-        ".1letter" = "1-letter (ARPABET) representations",
-        ".PrN" = "number of pronunciations",
-        ".br_IPA" = "IPA representations of words' British pronunciations",
+        ".1letter" = "1-letter (ARPABET) Representations",
+        ".PrN" = "Number of Pronunciations",
+        ".br_IPA" = "IPA Representations of Words' British Pronunciations",
         "Zipf." = "Frequency in Zipf",
         "fpmw." = "FPMW",
         "PoS." = "Part of Speech",
         "Length" = "Length",
-        "BG." = "bigram probability",
+        "BG." = "Bigram Probability",
         "ON.OLD20" = "OLD20",
         "ON.Colthearts_N" = "Coltheart's N",
         "ON.Log_OLD20" = "Log OLD20",
         "ON.Log_Colthearts_N" = "Log Coltheart's N",
-        "Syllables." = "number of syllables",
-        "Phonemes." = "number of phonemes",
-        "Rhyme." = "rhyme",
-        "FAM." = "familiarity ratings",
-        "AoA." = "AoA",
-        "CNC." = "concreteness ratings",
-        "IMAG." = "imageability ratings",
-        "AROU." = "arousal ratings",
-        "VAL." = "valence ratings",
-        "DOM." = "dominance ratings",
-        "SIZE." = "semantic size ratings",
-        "GEND." = "gender ratings",
-        "HUM." = "humour ratings",
-        "RT." = "lexical decision RT",
-        "Accuracy." = "lexical decision accuracy",
-        "PREV." = "word prevalence",
-        "PK." = "proportion known",
+        "Syllables." = "Number of Syllables",
+        "Phonemes." = "Number of Phonemes",
+        "Rhyme." = "Rhyme",
+        "FAM." = "Familiarity Ratings",
+        "AoA." = "Age of Acquisition",
+        "CNC." = "Concreteness Ratings",
+        "IMAG." = "Imageability Ratings",
+        "AROU." = "Arousal Ratings",
+        "VAL." = "Valence Ratings",
+        "DOM." = "Dominance Ratings",
+        "SIZE." = "Semantic Size Ratings",
+        "GEND." = "Gender Ratings",
+        "HUM." = "Humour Ratings",
+        "RT." = "Lexical Decision RT",
+        "Accuracy." = "Lexical Decision Accuracy",
+        "PREV." = "Word Prevalence",
+        "PK." = "Proportion Known",
         .default = default
       )
     }
   } else {
-    default
+    return(default)
   }
+  if (!title_caps) {
+    # regex to match sections of strings which should be converted to lower
+    lower_targs_regex <- "\\b(?!RT|OLD20|ARPABET|FPMW|Zipf|Coltheart|N$|IPA)\\S+"
+    # get the replacement lower case strings
+    rep_string <- regmatches(out, gregexpr(lower_targs_regex, out, perl = TRUE)) %>%
+      lapply(tolower)
+    # replace words that should be lower case with lower case versions
+    regmatches(out, gregexpr(lower_targs_regex, out, perl = TRUE)) <- rep_string
+  }
+  out
 }
 
 corpus_recode_name_measure <- function(var) {
