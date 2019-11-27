30b6157638c4abd3a3742bc83b15daffdda01e91
Jack Taylor <jackt@psy.gla.ac.uk>
2019-11-27 14:47:18 +0000

    only try render table when options are selected

--- inst/shiny-app/server/generate/cite.R
+++ inst/shiny-app/server/generate/cite.R
@@ -1,33 +1,37 @@
 # render a table that suggests sources that need citing
 output$gen_citations <- DT::renderDataTable({
 
-  # get the filters
-  filter_opts <- sapply(filter_opts_react(), function(x) x$var)
+  if (gen_splitby_boxes_N() > 0 | gen_controlfor_boxes_N() > 0 | gen_filterby_boxes_N() > 0) {
+    # get the splits
+    split_opts <- if (gen_splitby_boxes_N() > 0) sapply(split_opts_react(), function(x) x$var) else c()
 
-  # get the splits
-  split_opts <- sapply(split_opts_react(), function(x) x$var)
+    # get the controls
+    control_opts <- if (gen_controlfor_boxes_N() > 0) sapply(control_opts_react(), function(x) x$var) else c()
 
-  # get the controls
-  control_opts <- sapply(control_opts_react(), function(x) x$var)
+    # get the filters
+    filter_opts <- if (gen_filterby_boxes_N() > 0) sapply(filter_opts_react(), function(x) x$var) else c()
 
-  # get the table
-  c(filter_opts, split_opts, control_opts) %>%
-    LexOPS::citation_table() %>%
-    dplyr::mutate(url = ifelse(
-      is.na(url),
-      NA,
-      sprintf("<a href=\"%s\" target=\"_blank\", class=\"btn btn-primary\">Link to Source</a>", url)
-    )) %>%
-    dplyr::rename(
-      Variable = var,
-      Measure = measure,
-      Source = source,
-      Paper = url
-    ) %>%
-    DT::datatable(options = list(
-      paging = FALSE,
-      searching = FALSE,
-      dom = "t"
-    ), escape = FALSE)
+    # get the table
+    c(split_opts, control_opts, filter_opts) %>%
+      LexOPS::citation_table() %>%
+      dplyr::mutate(url = ifelse(
+        is.na(url),
+        NA,
+        sprintf("<a href=\"%s\" target=\"_blank\", class=\"btn btn-primary\">Link to Source</a>", url)
+      )) %>%
+      dplyr::rename(
+        Variable = var,
+        Measure = measure,
+        Source = source,
+        Paper = url
+      ) %>%
+      DT::datatable(options = list(
+        paging = FALSE,
+        searching = FALSE,
+        dom = "t"
+      ), escape = FALSE)
+  } else {
+    NULL
+  }
 
 })
--- inst/shiny-app/server/match_word/cite.R
+++ inst/shiny-app/server/match_word/cite.R
@@ -1,30 +1,34 @@
 # render a table that suggests sources that need citing
 output$match_citations <- DT::renderDataTable({
 
-  # get the filters
-  filter_opts <- sapply(match_filter_opts_react(), function(x) x$var)
+  if (match_filterby_boxes_N() > 0 | match_matchby_boxes_N() > 0) {
+    # get the filters
+    filter_opts <- if (match_filterby_boxes_N() > 0) sapply(match_filter_opts_react(), function(x) x$var) else c()
 
-  # get the matches
-  match_opts <- sapply(match_matchby_opts_react(), function(x) x$var)
+    # get the matches
+    match_opts <- if (match_matchby_boxes_N() > 0) sapply(match_matchby_opts_react(), function(x) x$var) else c()
 
-  # get the table
-  c(filter_opts, match_opts) %>%
-    LexOPS::citation_table() %>%
-    dplyr::mutate(url = ifelse(
-      is.na(url),
-      NA,
-      sprintf("<a href=\"%s\" target=\"_blank\", class=\"btn btn-primary\">Link to Source</a>", url)
-    )) %>%
-    dplyr::rename(
-      Variable = var,
-      Measure = measure,
-      Source = source,
-      Paper = url
-    ) %>%
-    DT::datatable(options = list(
-      paging = FALSE,
-      searching = FALSE,
-      dom = "t"
-    ), escape = FALSE)
+    # get the table
+    c(filter_opts, match_opts) %>%
+      LexOPS::citation_table() %>%
+      dplyr::mutate(url = ifelse(
+        is.na(url),
+        NA,
+        sprintf("<a href=\"%s\" target=\"_blank\", class=\"btn btn-primary\">Link to Source</a>", url)
+      )) %>%
+      dplyr::rename(
+        Variable = var,
+        Measure = measure,
+        Source = source,
+        Paper = url
+      ) %>%
+      DT::datatable(options = list(
+        paging = FALSE,
+        searching = FALSE,
+        dom = "t"
+      ), escape = FALSE)
+  } else {
+    NULL
+  }
 
 })
