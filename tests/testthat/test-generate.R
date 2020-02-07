a31edbe629402b15b14ee72b4d264240ca72195c
JackEdTaylor <33028594+JackEdTaylor@users.noreply.github.com>
2020-02-07 10:16:54 +0000

    test controls, reproducibility, and "balanced" match_null warning

--- tests/testthat/test-generate.R
+++ tests/testthat/test-generate.R
@@ -1,6 +1,6 @@
 context("generate")
 
-testthat::test_that("warnings", {
+testthat::test_that("general warnings", {
   testthat::expect_warning(
     lexops %>%
       split_by(Syllables.CMU, c(1, 3) ~ c(4, 6) ~ c(7, 20)) %>%
@@ -13,7 +13,7 @@ testthat::test_that("warnings", {
 
   testthat::expect_warning(
     lexops %>%
-      dplyr::filter(PK.Brysbaert >= .75) %>%
+      subset(PK.Brysbaert >= 0.75) %>%
       split_by(BG.SUBTLEX_UK, 0.001:0.003 ~ 0.009:0.011) %>%
       control_for(Zipf.SUBTLEX_UK, -0.2:0.2) %>%
       control_for(Length) %>%
@@ -21,9 +21,21 @@ testthat::test_that("warnings", {
     "No tolerance given for numeric variable 'Length', will control for exactly.",
     fixed = TRUE
   )
+
+  testthat::expect_warning(
+    lexops %>%
+      split_by(CNC.Brysbaert, 1:2 ~ 4:5) %>%
+      split_by(VAL.Warriner, 1:3 ~ 4.5:5.5 ~ 7:9) %>%
+      control_for(Zipf.SUBTLEX_UK, -0.25:0.25) %>%
+      control_for(Length, 0:0) %>%
+      control_for(PoS.BNC.Written) %>%
+      generate(n = "all", match_null = "balanced"),
+    '`match_null="balanced"` may not work when `n="all"`. Check distributions of match_null in the output.',
+    fixed = TRUE
+  )
 })
 
-testthat::test_that("errors", {
+testthat::test_that("general errors", {
   testthat::expect_error(
     generate(20),
     "Expected df to be of class data frame, not numeric",
@@ -57,7 +69,7 @@ testthat::test_that("errors", {
 
   testthat::expect_error(
     lexops %>%
-      dplyr::filter(PK.Brysbaert >= .75) %>%
+      subset(PK.Brysbaert >= 0.75) %>%
       split_by(BG.SUBTLEX_UK, 0.001:0.003 ~ 0.009:0.011) %>%
       control_for(Zipf.SUBTLEX_UK, -0.2:0.2) %>%
       control_for(Length, 0:0) %>%
@@ -67,15 +79,134 @@ testthat::test_that("errors", {
   )
 })
 
-testthat::test_that("standard and non-standard evaluation equivalence, and seed reproducibility", {
+testthat::test_that("reproducibility", {
+  # non-standard vs. non-standard (seed test)
   testthat::expect_identical(
     lexops %>%
-      split_by(Syllables.CMU, 1:3 ~ 4:6 ~ 7:20) %>%
-      control_for(Zipf.SUBTLEX_UK, -0.2:0.2) %>%
-      generate(n = 20, seed = 42),
+      split_by(CNC.Brysbaert, 1:2 ~ 4:5) %>%
+      split_by(VAL.Warriner, 1:3 ~ 4.5:5.5 ~ 7:9) %>%
+      control_for(Zipf.SUBTLEX_UK, -0.25:0.25) %>%
+      control_for(Length, 0:0) %>%
+      control_for(PoS.BNC.Written) %>%
+      generate(n = 10, seed = 42),
     lexops %>%
-      split_by("Syllables.CMU", list(c(1, 3), c(4, 6), c(7, 20)), standard_eval = TRUE) %>%
-      control_for("Zipf.SUBTLEX_UK", c(-0.2, 0.2), standard_eval = TRUE) %>%
-      generate(n = 20, seed = 42)
+      split_by(CNC.Brysbaert, 1:2 ~ 4:5) %>%
+      split_by(VAL.Warriner, 1:3 ~ 4.5:5.5 ~ 7:9) %>%
+      control_for(Zipf.SUBTLEX_UK, -0.25:0.25) %>%
+      control_for(Length, 0:0) %>%
+      control_for(PoS.BNC.Written) %>%
+      generate(n = 10, seed = 42)
+  )
+  # standard vs. standard (seed test)
+  testthat::expect_identical(
+    lexops %>%
+      split_by("CNC.Brysbaert", list(c(1, 2), c(4, 5)), standard_eval = TRUE) %>%
+      split_by("VAL.Warriner", list(c(1, 3), c(4.5, 5.5), c(7, 9)), standard_eval = TRUE) %>%
+      control_for("Zipf.SUBTLEX_UK", c(-0.25, 0.25), standard_eval = TRUE) %>%
+      control_for("Length", c(0, 0), standard_eval = TRUE) %>%
+      control_for("PoS.BNC.Written", standard_eval = TRUE) %>%
+      generate(n = 10, seed = 42),
+    lexops %>%
+      split_by("CNC.Brysbaert", list(c(1, 2), c(4, 5)), standard_eval = TRUE) %>%
+      split_by("VAL.Warriner", list(c(1, 3), c(4.5, 5.5), c(7, 9)), standard_eval = TRUE) %>%
+      control_for("Zipf.SUBTLEX_UK", c(-0.25, 0.25), standard_eval = TRUE) %>%
+      control_for("Length", c(0, 0), standard_eval = TRUE) %>%
+      control_for("PoS.BNC.Written", standard_eval = TRUE) %>%
+      generate(n = 10, seed = 42)
+  )
+  # non-standard vs. standard (transferability test)
+  testthat::expect_identical(
+    lexops %>%
+      split_by(CNC.Brysbaert, 1:2 ~ 4:5) %>%
+      split_by(VAL.Warriner, 1:3 ~ 4.5:5.5 ~ 7:9) %>%
+      control_for(Zipf.SUBTLEX_UK, -0.25:0.25) %>%
+      control_for(Length, 0:0) %>%
+      control_for(PoS.BNC.Written) %>%
+      generate(n = 10, seed = 42),
+    lexops %>%
+      split_by("CNC.Brysbaert", list(c(1, 2), c(4, 5)), standard_eval = TRUE) %>%
+      split_by("VAL.Warriner", list(c(1, 3), c(4.5, 5.5), c(7, 9)), standard_eval = TRUE) %>%
+      control_for("Zipf.SUBTLEX_UK", c(-0.25, 0.25), standard_eval = TRUE) %>%
+      control_for("Length", c(0, 0), standard_eval = TRUE) %>%
+      control_for("PoS.BNC.Written", standard_eval = TRUE) %>%
+      generate(n = 10, seed = 42)
+  )
+  # hybrid vs. hybrid (mixed transferability test)
+  testthat::expect_identical(
+    lexops %>%
+      split_by("CNC.Brysbaert", list(c(1, 2), c(4, 5)), standard_eval = TRUE) %>%
+      split_by(VAL.Warriner, 1:3 ~ 4.5:5.5 ~ 7:9) %>%
+      control_for("Zipf.SUBTLEX_UK", c(-0.25, 0.25), standard_eval = TRUE) %>%
+      control_for(Length, 0:0) %>%
+      control_for("PoS.BNC.Written", standard_eval = TRUE) %>%
+      generate(n = 10, seed = 42),
+    lexops %>%
+      split_by(CNC.Brysbaert, 1:2 ~ 4:5) %>%
+      split_by("VAL.Warriner", list(c(1, 3), c(4.5, 5.5), c(7, 9)), standard_eval = TRUE) %>%
+      control_for(Zipf.SUBTLEX_UK, -0.25:0.25) %>%
+      control_for("Length", c(0, 0), standard_eval = TRUE) %>%
+      control_for(PoS.BNC.Written) %>%
+      generate(n = 10, seed = 42)
+  )
+})
+
+testthat::test_that("controls", {
+  # test that categorical controls are applied correctly
+  testthat::expect_equal(
+    match_fail_count <- lexops %>%
+      subset(PK.Brysbaert >= 0.75) %>%
+      split_by(BG.SUBTLEX_UK, 0.001:0.003 ~ 0.009:0.011) %>%
+      control_for(PoS.BNC.Written) %>%
+      generate(n = 1000, match_null = "balanced") %>%
+      tidyr::pivot_longer(c(A1, A2), names_to = "condition", values_to = "string") %>%
+      dplyr::left_join(lexops, by = "string") %>%
+      dplyr::group_by(item_nr) %>%
+      dplyr::filter(length(unique(PoS.BNC.Written)) != 1) %>%
+      nrow(),
+    0
+  )
+  # test that exact numeric controls are applied correctly
+  testthat::expect_equal(
+    lexops %>%
+      subset(PK.Brysbaert >= 0.75) %>%
+      split_by(BG.SUBTLEX_UK, 0.001:0.003 ~ 0.009:0.011) %>%
+      control_for(Length, 0:0) %>%
+      generate(n = 1000, match_null = "balanced") %>%
+      tidyr::pivot_longer(c(A1, A2), names_to = "condition", values_to = "string") %>%
+      dplyr::left_join(lexops, by = "string") %>%
+      dplyr::group_by(item_nr) %>%
+      dplyr::filter(length(unique(Length)) != 1) %>%
+      nrow(),
+    0
+  )
+  # test that inexact integer numeric controls are applied correctly
+  testthat::expect_equal(
+    lexops %>%
+      subset(PK.Brysbaert >= 0.75) %>%
+      split_by(BG.SUBTLEX_UK, 0.001:0.003 ~ 0.009:0.011) %>%
+      control_for(Length, -1:1) %>%
+      generate(n = 1000, match_null = "balanced") %>%
+      tidyr::pivot_longer(c(A1, A2), names_to = "condition", values_to = "string") %>%
+      dplyr::left_join(lexops, by = "string") %>%
+      dplyr::group_by(item_nr) %>%
+      dplyr::summarise(max_diff = abs(max(Length) - min(Length))) %>%
+      dplyr::filter(max_diff > 1) %>%
+      nrow(),
+    0
+  )
+  # test that inexact float numeric controls are applied correctly
+  testthat::expect_equal(
+    lexops %>%
+      subset(PK.Brysbaert >= 0.75) %>%
+      split_by(BG.SUBTLEX_UK, 0.001:0.003 ~ 0.009:0.011) %>%
+      control_for(Zipf.BNC.All, -0.2:0.2) %>%
+      generate(n = 1000, match_null = "balanced") %>%
+      tidyr::pivot_longer(c(A1, A2), names_to = "condition", values_to = "string") %>%
+      dplyr::left_join(lexops, by = "string") %>%
+      dplyr::group_by(item_nr) %>%
+      dplyr::summarise(max_diff = abs(max(Zipf.BNC.All) - min(Zipf.BNC.All))) %>%
+      dplyr::filter(max_diff > 0.2) %>%
+      nrow(),
+    0
   )
 })
