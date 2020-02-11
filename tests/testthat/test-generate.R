1c95be1a64c19d2c8875366f0735026ae924cd87
JackEdTaylor <33028594+JackEdTaylor@users.noreply.github.com>
2020-02-11 09:22:35 +0000

    labelled tests for most important functionality

--- tests/testthat/test-generate.R
+++ tests/testthat/test-generate.R
@@ -1,5 +1,6 @@
 context("generate")
 
+# general warnings ----
 testthat::test_that("general warnings", {
   testthat::expect_warning(
     lexops %>%
@@ -35,6 +36,7 @@ testthat::test_that("general warnings", {
   )
 })
 
+# general errors ----
 testthat::test_that("general errors", {
   testthat::expect_error(
     generate(20),
@@ -79,6 +81,7 @@ testthat::test_that("general errors", {
   )
 })
 
+# reproducibility ----
 testthat::test_that("reproducibility", {
   # non-standard vs. non-standard (seed test)
   testthat::expect_identical(
@@ -150,10 +153,184 @@ testthat::test_that("reproducibility", {
   )
 })
 
+# splits ----
+testthat::test_that("splits", {
+  # test that categorical splits are applied correctly
+  testthat::expect_equal(
+    lexops %>%
+      subset(PK.Brysbaert >= 0.9) %>%
+      split_by(PoS.BNC.All, "substantive" ~ "verb") %>%
+      control_for(Zipf.BNC.All, -0.25:0.25) %>%
+      generate(1000) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, PoS.BNC.All) %>%
+          dplyr::rename(A1_PoS = PoS.BNC.All),
+        by = c("A1" = "string")
+      ) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, PoS.BNC.All) %>%
+          dplyr::rename(A2_PoS = PoS.BNC.All),
+        by = c("A2" = "string")
+      ) %>%
+      dplyr::filter(
+        A1_PoS == "substantive",
+        A2_PoS == "verb"
+      ) %>%
+      nrow(),
+    1000
+  )
+
+  # test that exact integer splits are applied correctly
+  testthat::expect_equal(
+    lexops %>%
+      subset(PK.Brysbaert >= 0.9) %>%
+      split_by(Length, 4:4 ~ 5:5) %>%
+      control_for(Zipf.BNC.All, -0.25:0.25) %>%
+      generate(1000) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Length) %>%
+          dplyr::rename(A1_Length = Length),
+        by = c("A1" = "string")
+      ) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Length) %>%
+          dplyr::rename(A2_Length = Length),
+        by = c("A2" = "string")
+      ) %>%
+      dplyr::filter(
+        A1_Length == 4,
+        A2_Length == 5
+      ) %>%
+      nrow(),
+    1000
+  )
+
+  # test that inexact, assymmetric integer splits are applied correctly
+  testthat::expect_equal(
+    lexops %>%
+      subset(PK.Brysbaert >= 0.9) %>%
+      split_by(Length, 3:4 ~ 5:12) %>%
+      control_for(Zipf.BNC.All, -0.25:0.25) %>%
+      generate(1000) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Length) %>%
+          dplyr::rename(A1_Length = Length),
+        by = c("A1" = "string")
+      ) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Length) %>%
+          dplyr::rename(A2_Length = Length),
+        by = c("A2" = "string")
+      ) %>%
+      dplyr::filter(
+        dplyr::between(A1_Length, 3, 4),
+        dplyr::between(A2_Length, 5, 12)
+      ) %>%
+      nrow(),
+    1000
+  )
+
+  # test that inexact, assymmetric float splits are applied correctly
+  testthat::expect_equal(
+    lexops %>%
+      subset(PK.Brysbaert >= 0.9) %>%
+      split_by(Zipf.BNC.All, 0.8:2.1 ~ 2.5:6) %>%
+      control_for(Length, 0:0) %>%
+      generate(1000) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Zipf.BNC.All) %>%
+          dplyr::rename(A1_Zipf = Zipf.BNC.All),
+        by = c("A1" = "string")
+      ) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Zipf.BNC.All) %>%
+          dplyr::rename(A2_Zipf = Zipf.BNC.All),
+        by = c("A2" = "string")
+      ) %>%
+      dplyr::filter(
+        dplyr::between(A1_Zipf, 0.8, 2.1),
+        dplyr::between(A2_Zipf, 2.5, 6)
+      ) %>%
+      nrow(),
+    1000
+  )
+
+  # test that splits can be combined
+  testthat::expect_equal(
+    lexops %>%
+      subset(PK.Brysbaert >= 0.9) %>%
+      split_by(Zipf.BNC.All, 0.8:2.1 ~ 2.5:6) %>%
+      split_by(PoS.BNC.All, "substantive" ~ "verb" ~ "adjective") %>%
+      control_for(Length, 0:0) %>%
+      generate(100) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Zipf.BNC.All, PoS.BNC.All) %>%
+          dplyr::rename(A1_B1_Zipf = Zipf.BNC.All, A1_B1_PoS = PoS.BNC.All),
+        by = c("A1_B1" = "string")
+      ) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Zipf.BNC.All, PoS.BNC.All) %>%
+          dplyr::rename(A1_B2_Zipf = Zipf.BNC.All, A1_B2_PoS = PoS.BNC.All),
+        by = c("A1_B2" = "string")
+      ) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Zipf.BNC.All, PoS.BNC.All) %>%
+          dplyr::rename(A1_B3_Zipf = Zipf.BNC.All, A1_B3_PoS = PoS.BNC.All),
+        by = c("A1_B3" = "string")
+      ) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Zipf.BNC.All, PoS.BNC.All) %>%
+          dplyr::rename(A2_B1_Zipf = Zipf.BNC.All, A2_B1_PoS = PoS.BNC.All),
+        by = c("A2_B1" = "string")
+      ) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Zipf.BNC.All, PoS.BNC.All) %>%
+          dplyr::rename(A2_B2_Zipf = Zipf.BNC.All, A2_B2_PoS = PoS.BNC.All),
+        by = c("A2_B2" = "string")
+      ) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Zipf.BNC.All, PoS.BNC.All) %>%
+          dplyr::rename(A2_B3_Zipf = Zipf.BNC.All, A2_B3_PoS = PoS.BNC.All),
+        by = c("A2_B3" = "string")
+      ) %>%
+      dplyr::filter(
+        dplyr::between(A1_B1_Zipf, 0.8, 2.1),
+        dplyr::between(A1_B2_Zipf, 0.8, 2.1),
+        dplyr::between(A1_B3_Zipf, 0.8, 2.1),
+        dplyr::between(A2_B1_Zipf, 2.5, 6),
+        dplyr::between(A2_B2_Zipf, 2.5, 6),
+        dplyr::between(A2_B3_Zipf, 2.5, 6),
+        A1_B1_PoS == "substantive",
+        A1_B2_PoS == "verb",
+        A1_B3_PoS == "adjective",
+        A2_B1_PoS == "substantive",
+        A2_B2_PoS == "verb",
+        A2_B3_PoS == "adjective"
+      ) %>%
+      nrow(),
+    100
+  )
+})
+
+# controls ----
 testthat::test_that("controls", {
   # test that categorical controls are applied correctly
   testthat::expect_equal(
-    match_fail_count <- lexops %>%
+    lexops %>%
       subset(PK.Brysbaert >= 0.75) %>%
       split_by(BG.SUBTLEX_UK, 0.001:0.003 ~ 0.009:0.011) %>%
       control_for(PoS.BNC.Written) %>%
@@ -161,9 +338,9 @@ testthat::test_that("controls", {
       tidyr::pivot_longer(c(A1, A2), names_to = "condition", values_to = "string") %>%
       dplyr::left_join(lexops, by = "string") %>%
       dplyr::group_by(item_nr) %>%
-      dplyr::filter(length(unique(PoS.BNC.Written)) != 1) %>%
+      dplyr::filter(length(unique(PoS.BNC.Written)) == 1) %>%
       nrow(),
-    0
+    2000  # as pivot_longer but no group_by(item_nr)
   )
   # test that exact numeric controls are applied correctly
   testthat::expect_equal(
@@ -175,9 +352,9 @@ testthat::test_that("controls", {
       tidyr::pivot_longer(c(A1, A2), names_to = "condition", values_to = "string") %>%
       dplyr::left_join(lexops, by = "string") %>%
       dplyr::group_by(item_nr) %>%
-      dplyr::filter(length(unique(Length)) != 1) %>%
+      dplyr::filter(length(unique(Length)) == 1) %>%
       nrow(),
-    0
+    2000  # as pivot_longer but no group_by(item_nr)
   )
   # test that inexact integer numeric controls are applied correctly
   testthat::expect_equal(
@@ -190,9 +367,9 @@ testthat::test_that("controls", {
       dplyr::left_join(lexops, by = "string") %>%
       dplyr::group_by(item_nr) %>%
       dplyr::summarise(max_diff = abs(max(Length) - min(Length))) %>%
-      dplyr::filter(max_diff > 1) %>%
+      dplyr::filter(dplyr::between(max_diff, 0, 1)) %>%
       nrow(),
-    0
+    1000
   )
   # test that inexact float numeric controls are applied correctly
   testthat::expect_equal(
@@ -205,8 +382,136 @@ testthat::test_that("controls", {
       dplyr::left_join(lexops, by = "string") %>%
       dplyr::group_by(item_nr) %>%
       dplyr::summarise(max_diff = abs(max(Zipf.BNC.All) - min(Zipf.BNC.All))) %>%
-      dplyr::filter(max_diff > 0.2) %>%
+      dplyr::filter(max_diff <= 0.2) %>%
       nrow(),
-    0
+    1000
   )
+  # test that inexact, assymmetric float numeric controls are applied correctly
+  testthat::expect_equal(
+    lexops %>%
+      subset(PK.Brysbaert >= 0.75) %>%
+      split_by(BG.SUBTLEX_UK, 0.001:0.003 ~ 0.009:0.011) %>%
+      control_for(Zipf.BNC.All, -0.1:0.25) %>%
+      generate(n = 1000, match_null = "balanced") %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Zipf.BNC.All) %>%
+          dplyr::rename(A1_Zipf = Zipf.BNC.All),
+        by = c("A1" = "string")
+      ) %>%
+      dplyr::left_join(
+        lexops %>%
+          dplyr::select(string, Zipf.BNC.All) %>%
+          dplyr::rename(A2_Zipf = Zipf.BNC.All),
+        by = c("A2" = "string")
+      ) %>%
+      dplyr::mutate(Zipf_diff = ifelse(match_null == "A1", A2_Zipf - A1_Zipf, A1_Zipf - A2_Zipf)) %>%
+      dplyr::filter(dplyr::between(Zipf_diff, -0.1, 0.25)) %>%
+      nrow(),
+    1000
+  )
+})
+
+# match_null options ----
+testthat::test_that("match_null options", {
+  # check balanced match nulls exist in equal frequencies for n divisible by number of conds
+  testthat::expect_true({
+    match_null_counts <- lexops %>%
+      subset(PK.Brysbaert >= 0.75) %>%
+      split_by(BG.SUBTLEX_UK, 0.001:0.003 ~ 0.009:0.011) %>%
+      control_for(Zipf.BNC.All, -0.2:0.2) %>%
+      generate(n = 1000, match_null = "balanced") %>%
+      dplyr::group_by(match_null) %>%
+      dplyr::count() %>%
+      dplyr::pull(n)
+    all(match_null_counts == 500)
+  })
+  # check balanced match nulls exist in almost equal frequencies for n not divisible by number of conds
+  testthat::expect_true({
+    match_null_counts <- lexops %>%
+      subset(PK.Brysbaert >= 0.75) %>%
+      split_by(BG.SUBTLEX_UK, 0.001:0.003 ~ 0.009:0.011) %>%
+      control_for(Zipf.BNC.All, -0.2:0.2) %>%
+      generate(n = 1003, match_null = "balanced") %>%
+      dplyr::group_by(match_null) %>%
+      dplyr::count() %>%
+      dplyr::pull(n)
+    (501 %in% match_null_counts) & (502 %in% match_null_counts)
+  })
+  # check "first" match nulls work
+  testthat::expect_true({
+    match_nulls <- lexops %>%
+      split_by(CNC.Brysbaert, 1:2 ~ 4:5) %>%
+      split_by(VAL.Warriner, 1:3 ~ 4.5:5.5 ~ 7:9) %>%
+      control_for(Zipf.SUBTLEX_UK, -0.25:0.25) %>%
+      control_for(Length, 0:0) %>%
+      control_for(PoS.BNC.Written) %>%
+      generate(n = 10, match_null = "first") %>%
+      dplyr::pull(match_null)
+    all(match_nulls == "A1_B1")
+  })
+  # check specific match nulls can be selected
+  testthat::expect_true({
+    match_nulls <- lexops %>%
+      split_by(CNC.Brysbaert, 1:2 ~ 4:5) %>%
+      split_by(VAL.Warriner, 1:3 ~ 4.5:5.5 ~ 7:9) %>%
+      control_for(Zipf.SUBTLEX_UK, -0.25:0.25) %>%
+      control_for(Length, 0:0) %>%
+      control_for(PoS.BNC.Written) %>%
+      generate(n = 10, match_null = "A2_B3") %>%
+      dplyr::pull(match_null)
+    all(match_nulls == "A2_B3")
+  })
+  # check inclusive match nulls work
+  testthat::expect_equal(
+    lexops %>%
+      split_by(CNC.Brysbaert, 1:2 ~ 4:5) %>%
+      split_by(VAL.Warriner, 1:3 ~ 4.5:5.5 ~ 7:9) %>%
+      control_for(Zipf.SUBTLEX_UK, -0.5:0.5) %>%
+      control_for(BG.SUBTLEX_UK, -0.05:0.9) %>%
+      generate(n = 10, match_null = "inclusive") %>%
+      long_format() %>%
+      dplyr::group_by(item_nr) %>%
+      dplyr::filter(
+        max(Zipf.SUBTLEX_UK) - min(Zipf.SUBTLEX_UK) <= 0.5,
+        # note that assymmetric tolerances are cut short to become symmmetrical, with a maximum extent of the shortest distance from zero of the supplied positive and negative tolernces. As a result, -0.05:0.9 becomes -0.05:0.05
+        max(BG.SUBTLEX_UK) - min(BG.SUBTLEX_UK) <= 0.05
+      ) %>%
+      nrow(),
+    60
+  )
+})
+
+# control_for_map ----
+testthat::test_that("control_for_map", {
+  # test with two conditions
+  testthat::expect_equal({
+    library(vwr)
+    lexops %>%
+      split_by(AROU.Warriner, 1:3 ~ 7:9) %>%
+      control_for_map(levenshtein.distance, string, 0:2) %>%
+      generate(10) %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(ld = levenshtein.distance(A1, A2)) %>%
+      dplyr::filter(ld <= 2) %>%
+      nrow()
+  }, 10)
+  # test with three conditions
+  testthat::expect_equal({
+    library(vwr)
+    lexops %>%
+      split_by(PoS.BNC.All, "substantive" ~ "verb" ~ "adjective") %>%
+      control_for_map(levenshtein.distance, string, 0:3) %>%
+      generate(50) %>%
+      dplyr::mutate(match_null_string = dplyr::case_when(
+        match_null == "A1" ~ A1,
+        match_null == "A2" ~ A2,
+        match_null == "A3" ~ A3
+      )) %>%
+      tidyr::pivot_longer(c(A1, A2, A3), names_to = "condition", values_to = "string") %>%
+      dplyr::rowwise() %>%
+      dplyr::mutate(ld = levenshtein.distance(match_null_string, string)) %>%
+      dplyr::filter(ld <= 3) %>%
+      nrow()
+  }, 150)
 })
