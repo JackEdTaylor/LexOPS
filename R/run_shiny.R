122d3497b18debda74300fc4f061bd2a91ab4976
JackEdTaylor <33028594+JackEdTaylor@users.noreply.github.com>
2020-02-26 12:16:37 +0000

    suggest but don't import packages which are mostly only used in the shiny app

--- R/run_shiny.R
+++ R/run_shiny.R
@@ -2,15 +2,38 @@
 #'
 #' Runs the LexOPS Shiny App, with optional additional arguments.
 #'
+#' The LexOPS shiny app requires additional packages. If any dependencies are missing, the user will be given an error and installation instructions. The additional packages required are:
+#' * `shiny`
+#' * `shinydashboard`
+#' * `plotly`
+#' * `ggwordcloud`
+#' * `colourpicker`
+#' * `shinycssloaders`
+#' * `shinyjs`
+#' * `viridis`
+#'
 #' @param ... optional arguments to `shiny::runApp()`
 #'
 #' @export
 
 run_shiny <- function(...) {
   appDir <- system.file("shiny-app", package = "LexOPS")
+  # check app is installed
   if (appDir == "") {
     stop("Could not find the directory for the LexOPS Shiny App. Try re-installing `LexOPS`.")
   }
+  # check dependencies
+  shiny_deps <- c("shiny", "shinydashboard", "plotly", "ggwordcloud", "colourpicker", "shinycssloaders", "shinyjs", "viridis")
+  shiny_deps_missing <- lapply(shiny_deps, function(pkg) {
+    if (!requireNamespace(pkg, quietly = TRUE)) TRUE else FALSE
+  })
+  # if missing any dependencies, stop and tell the user how to install
+  if (any(unlist(shiny_deps_missing))) {
+    deps_vec <- shiny_deps[shiny_deps_missing == TRUE]
+    deps_inst_str <- sprintf("install.packages(c(%s))", paste(sprintf("\"%s\"", deps_vec), collapse=", "))
+    stop(sprintf("You are missing %s packages required by the LexOPS shiny app. Install them with:\n%s", length(deps_vec), deps_inst_str))
+  }
+  # run app
   shiny::runApp(appDir, ...)
 }
 
