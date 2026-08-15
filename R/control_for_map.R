e1e88e6dbb729a7e0225f1d39970e75fdbe4c0a1
Jack Taylor <taylor@psych.uni-frankfurt.de>
2026-08-15 19:25:20 +0200

    deprecated error

--- R/control_for_map.R
+++ R/control_for_map.R
@@ -117,12 +117,6 @@ control_for_map <- function(x, fun, var, tol = NA, name = NA, standard_eval = FA
     prepend(var, name)
   }
 
-  # check that the conditions are present in the attributes
-  if (is.null(cond_col)) {
-    # if the column containing the condition info is missing and not defined manually, throw error
-    stop("Could not identify split conditions column! Make sure you run split_by() before generate().")
-  }
-
   # add the specified controls to df's attributes
   if (is.null(lp_info$control_functions)) {
     lp_info$control_functions <- list(var)
